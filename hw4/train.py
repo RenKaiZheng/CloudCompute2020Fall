6f5a7f534bcce9be0e7d4abc90fab63ff6c23781
RenKaiZheng <43511153+RenKaiZheng@users.noreply.github.com>
2020-11-26 15:44:33 +0800

    Add files via upload

--- hw4/train.py
+++ hw4/train.py
@@ -1,75 +1,95 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 import sys
 import os
-SPARK_HOME = "/usr/local/spark-0.9.1"
-os.environ["SPARK_HOME"] = SPARK_HOME
-os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
-sys.path.append(SPARK_HOME + "/python")
-
-
-from pyspark.mllib.classification import LogisticRegressionWithSGD
 import numpy as np
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
 
 
-def getSparkContext():
-    """
-    Gets the Spark Context
-    """
-    conf = (SparkConf()
-         .setMaster("local") # run on local
-         .setAppName("Logistic Regression") # Name of App
-         .set("spark.executor.memory", "1g")) # Set 1 gig of memory
-    sc = SparkContext(conf = conf) 
-    return sc
+# In[2]:
 
-def mapper(line):
-    """
-    Mapper that converts an input line to a feature vector
-    """    
-    feats = line.strip().split(",") 
-    # labels must be at the beginning for LRSGD
-    label = feats[len(feats) - 1] 
-    feats = feats[: len(feats) - 1]
-    feats.insert(0,label)
-    features = [ float(feature) for feature in feats ] # need floats
-    return np.array(features)
 
-sc = getSparkContext()
+datasetPath = 'gs://hw4_spark_b06902127/data_banknote_authentication.txt'
+iteration = 1000
+D = 5
 
-# Load and parse the data
-data = sc.textFile("hdfs://localhost/user/hduser2/data")
-parsedData = data.map(mapper)
+w = 2 * np.random.ranf(size=D) - 1
+lr = 0.001
 
-'''
-data = spark.textFile(...).map(readPoint).cache()
-w = Vector.random(D)
-for (i <- 1 to ITERATIONS) {
-    val gradient = data.map(p =>
-        (1 / (1 + exp(-p.y*(w dot p.x))) - p.y) * p.y * p.x
-    ).reduce(_ + _)
-    w -= gradient
-}
-'''
 
-# Train model
-model = LogisticRegressionWithSGD.train(parsedData)
+# In[33]:
 
-# Predict the first elem will be actual data and the second 
-# item will be the prediction of the model
-labelsAndPreds = parsedData.map(lambda point: (int(point.item(0)), 
-        model.predict(point.take(range(1, point.size)))))
 
-# Evaluating the model on training data
-trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
+def readPointBatch(iterator):
+    strs = list(iterator)
+    matrix = np.ones((len(strs), D + 1))
+    for i, s in enumerate(strs):
+        matrix[i, :-1] = np.fromstring(s.replace(',', ' '), dtype=np.float32, sep=' ')
+        label = matrix[i, 4]
+        matrix[i, 4] = matrix[i, 0]
+        matrix[i, 0] = label
+    return matrix
 
-'''
-threshold = 0.5
+
+# In[34]:
+
+
+spark = SparkSession.builder.appName("spark").getOrCreate()
+points = spark.read.text(datasetPath).rdd.map(lambda r: r[0]).mapPartitions(readPointBatch).cache()
+
+
+# In[91]:
+
+
 def sigmoid(x):
     return 1.0/(np.exp(-x) + 1.0)
-for i in range(10000):
-    gradient = -(train_x.T.dot(train_y - sigmoid(train_x.dot(w))))
-    w = w - lr*gradient#/(np.sqrt(ada_sum) + 0.00000001)
-'''
 
-# Print some stuff
-print("Training Error = " + str(trainErr))
+def gradient(matrix, w):
+    Y = matrix[:, 0]
+    X = matrix[:, 1:]
+    return -(X.T.dot(Y - sigmoid(X.dot(w))))
+
+def add(x, y):
+    return x + y
+
+def predict(matrix, w):
+    X = matrix[:, 1:]
+    Y = matrix[:, 0]
+    result = sigmoid(X.dot(w))
+    return result, Y
+
+
+# In[52]:
+
+
+for i in range(iteration):
+    grad = points.map(lambda m: gradient(m, w)).reduce(add)
+    w -= lr*grad
+
+
+# In[102]:
+
+
+result = points.map(lambda m: predict(m, w))
+pred, label = result.collect()[0]
+acc = 0.
+for i in range(len(pred)):
+    p = pred[i]
+    l = label[i]
+    if p > 0.5 and l > 0:
+        acc += 1
+    elif p <= 0.5 and l < 1:
+        acc += 1
+print acc/len(pred)
+
+
+# In[103]:
+
+
+print(w)
+
